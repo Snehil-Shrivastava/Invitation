9ef6fb4beebd21336b1284b589a17c591bba7319
Snehil-Shrivastava <snehil.shrivastava@xvscreations.com>
2026-02-11 15:42:14 +0530

    wedding invitation clone

---
+++ app/junk/Home.jsx
@@ -0,0 +1,55 @@
+// <div className="w-full h-full p-4 grainy-background relative font-gillian-regular ">
+//   <div className="w-full h-full border-2 border-amber-500 pointer-events-none rounded-tl-[56px]" />
+//   <div className="absolute -right-80 -top-50 animate-sway z-1">
+//     <Image src={flower} alt="image" width={490} height={373} />
+//   </div>
+//   <div
+//     className="absolute -right-25 -top-5 rotate-z-200 animate-sway"
+//     style={{
+//       // @ts-expect-error custom style
+//       "--base-rotation": "200deg",
+//     }}
+//   >
+//     <Image src={leaf} alt="leaf" width={250} height={373} />
+//   </div>
+//   <div
+//     className="absolute -top-10 -left-45 animate-sway"
+//     style={{
+//       // @ts-expect-error custom style
+//       "--sway-duration": "1.5s",
+//       "--sway-angle": "-2deg",
+//     }}
+//   >
+//     <Image src={flowerWithLeaf} alt="image" width={390} height={373} />
+//   </div>
+//   <div className="absolute top-0 inset-x-0 h-full max-h-163.75">
+//     <h1 className="text-center text-[#c53974] font-semibold text-4xl relative top-1/2 -translate-y-1/2">
+//       John weds Jane
+//     </h1>
+//   </div>
+//   <div
+//     className="absolute -left-15 top-100 rotate-z-50 animate-sway"
+//     style={{
+//       // @ts-expect-error custom style
+//       "--base-rotation": "50deg",
+//       "--sway-duration": "1s",
+//       "--sway-angle": "-2deg",
+//     }}
+//   >
+//     <Image src={leaf} alt="leaf" width={180} height={373} />
+//   </div>
+//   <div
+//     className="absolute -right-75 top-70 -rotate-z-12 animate-sway"
+//     style={{
+//       // @ts-expect-error custom style
+//       "--base-rotation": "-12deg",
+//       "--sway-duration": "2.5s",
+//       "--sway-angle": "-2deg",
+//     }}
+//   >
+//     <Image src={flowerWithNoLeaf} alt="leaf" width={520} height={570} />
+//   </div>
+//   <div className="absolute top-165 inset-x-0 text-center text-[#d26d3c]">
+//     <span className="text-flash">scroll down</span>
+//   </div>
+// </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,288 @@
 import Image from "next/image";
+import flower from "@/public/flower.svg";
+import leaf from "@/public/leaf.avif";
+import leaf2 from "@/public/leaf_2.svg";
+import flowerWithLeaf from "@/public/flower_with_leaf.svg";
+import flowerWithNoLeaf from "@/public/flower_no_leaf.svg";
+import flowerGroup from "@/public/flower_group.avif";
+import flowerFragment from "@/public/flower_fragment.svg";
+import flowerClosed from "@/public/flower_closed.svg";
+import buddha from "@/public/buddha.avif";
+import eventTime from "@/public/decorative_time.avif";
+import haldi from "@/public/haldi.avif";
+import hangingFlower from "@/public/hanging_flowers.avif";
+import peacock from "@/public/peacock.avif";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
+    <main className="relative w-full min-h-screen overflow-hidden p-4 font-gillian-regular">
+      <div className="absolute inset-4 border-2 border-amber-500 pointer-events-none z-5" />
+      <div className="absolute inset-0 z-10 pointer-events-none grainy-background mix-blend-overlay" />
+
+      {/* 4. Content Container */}
+      <div className="relative w-full h-full z-8">
+        <div className="absolute -right-82 -top-50 animate-sway z-1 pointer-events-none">
+          <Image
+            src={flower}
+            alt="image"
+            width={490}
+            height={373}
+            className="scale-120"
+          />
+        </div>
+        <div
+          className="absolute -right-25 -top rotate-z-200 animate-sway pointer-events-none"
+          style={{
+            // @ts-expect-error custom style
+            "--base-rotation": "200deg",
+          }}
+        >
+          <Image src={leaf} alt="leaf" width={250} height={373} />
+        </div>
+        <div
+          className="absolute -top-10 -left-45 animate-sway pointer-events-none"
+          style={{
+            // @ts-expect-error custom style
+            "--sway-duration": "1.5s",
+            "--sway-angle": "-2deg",
+          }}
+        >
+          <Image src={flowerWithLeaf} alt="image" width={390} height={373} />
+        </div>
+        <div className="h-screen max-h-160 pointer-events-none">
+          <h1 className="text-center text-[#c53974] font-semibold text-4xl relative top-1/2 -translate-y-1/2">
+            John weds Jane
           </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+        <div
+          className="absolute -left-15 top-100 rotate-z-50 animate-sway pointer-events-none"
+          style={{
+            // @ts-expect-error custom style
+            "--base-rotation": "50deg",
+            "--sway-duration": "1s",
+            "--sway-angle": "-2deg",
+          }}
+        >
+          <Image src={leaf} alt="leaf" width={180} height={373} />
+        </div>
+        <div
+          className="absolute -right-75 top-70 -rotate-z-12 animate-sway pointer-events-none"
+          style={{
+            // @ts-expect-error custom style
+            "--base-rotation": "-12deg",
+            "--sway-duration": "2.5s",
+            "--sway-angle": "-2deg",
+          }}
+        >
+          <Image src={flowerWithNoLeaf} alt="leaf" width={520} height={570} />
+        </div>
+        <div className="relative -top-5 text-center text-[#d26d3c]">
+          <span className="text-flash">scroll down</span>
+        </div>
+        <div className="pb-10 w-[52%] mx-auto">
+          <div className="pt-10">
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
+              src={buddha}
+              alt="buddha"
+              width={216}
+              height={212}
+              className="mx-auto"
             />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+          </div>
+          <div lang="hi" className="text-center text-[#b29862] pt-2 pb-4">
+            <p className="font-jaini text-2xl">भवतु सब्ब मंगलम्</p>
+          </div>
+          <div className="text-[10px] text-center text-[#728e1c]">
+            <p>
+              with the blessings of Late Smt. Meerabai & Late Shri Ramcharan
+              Jawarker
+            </p>
+          </div>
+          <div className="text-[#c53974] font-semibold text-4xl text-center py-5 leading-20">
+            <h2>Jawarker</h2>
+          </div>
+          <div className="text-base text-center text-[#728e1c]">
+            <p>
+              family request the honor of your presence at the marriage of their
+              daughter
+            </p>
+          </div>
         </div>
-      </main>
-    </div>
+        <div className="relative pt-5 pb-15">
+          {/* <div className="h-100 w-70 relative -left-20 bg-[#919d6b] rounded-full">
+            <Image
+              src={flowerGroup}
+              alt="flower group"
+              width={512}
+              height={918}
+              className="relative -bottom-10 -right-12 scale-145"
+            />
+          </div> */}
+          {/* <div className="h-100 w-70 relative -left-20 bg-[#919d6b] rounded-full z-10"> */}
+          {/* <div
+              className="absolute inset-0 rounded-full"
+              style={{ clipPath: "circle(58% at 50% 50%)" }}
+            > */}
+          {/* <Image
+              src={flowerGroup}
+              alt="flower group"
+              width={512}
+              height={918}
+              className="relative -botom-10 -right-12 scale-145"
+            /> */}
+          {/* </div> */}
+          {/* </div> */}
+          <div className="h-100 w-70 relative -left-20 bg-[#919d6b] rounded-full">
+            <Image
+              src={flowerGroup}
+              alt="flower group"
+              width={512}
+              height={918}
+              className="relative -bottom-10 -right-12 scale-145"
+            />
+          </div>
+        </div>
+        <div className="w-[70%] mx-auto">
+          <div className="text-[#c53974] font-semibold text-4xl text-center leading-20">
+            <h3>Ishi Jawarker</h3>
+          </div>
+          <div className="text-base text-center text-[#728e1c]">
+            <p>d/o Mrs. Sunita & Mr. Dharam Das Jawarker</p>
+          </div>
+          <div className="text-[#b29862] font-semibold text-4xl text-center py-15 leading-20">
+            <h3>Weds</h3>
+          </div>
+          <div className="text-[#c53974] font-semibold text-4xl text-center leading-14">
+            <h3>Aishwary Khobragade</h3>
+          </div>
+          <div className="text-base text-center text-[#728e1c] py-5">
+            <p>s/o Mrs. Jayshree & Mr. Aekant Khobragade</p>
+          </div>
+          <div className="text-xs text-center text-[#728e1c] w-4/5 mx-auto">
+            <p>
+              grandson of Late Smt. Nagoobai & Late Shri Tulsidas Khobragade
+            </p>
+          </div>
+          <div>
+            <Image
+              src={eventTime}
+              alt=""
+              width={1204}
+              height={1024}
+              className="scale-110"
+            />
+          </div>
+          <div className="h-150 w-150 relative">
+            <div
+              className="relative -right-25 animate-sway"
+              style={{
+                // @ts-expect-error custom style
+                "--sway-duration": "2s",
+                "--sway-angle": "-2deg",
+              }}
+            >
+              <Image
+                src={flowerFragment}
+                alt="flower"
+                width={612}
+                height={577}
+              />
+            </div>
+            <div
+              className="absolute top-50 -left-27 bottom-0 animate-sway"
+              style={{
+                // @ts-expect-error custom style
+                "--sway-duration": "1.5s",
+                "--sway-angle": "-2deg",
+              }}
+            >
+              <Image src={flowerClosed} alt="flower closed" />
+            </div>
+            <div
+              className="absolute top-75 -left-35 animate-sway"
+              style={{
+                // @ts-expect-error custom style
+                "--sway-duration": "5s",
+                "--sway-angle": "5deg",
+              }}
+            >
+              <Image src={leaf2} alt="leaf" />
+            </div>
+          </div>
+          <div className="text-[#c53974] font-semibold text-2xl text-center leading-10">
+            <h4>Looking forward to welcome you</h4>
+          </div>
+          <div className="text-base text-center text-[#728e1c] py-5 w-4/5 mx-auto leading-8">
+            <p>Dr. Rohie Jawarker and Jawarker family</p>
+          </div>
+        </div>
+        <div className="pb-20">
+          <div className="h-80">
+            <div
+              className="-right-60 relative animate-sway"
+              style={{
+                // @ts-expect-error custom style
+                "--base-rotation": "-40deg",
+                "--sway-duration": "2s",
+                "--sway-angle": "-2deg",
+              }}
+            >
+              <Image src={leaf} alt="leaf" width={200} height={373} />
+            </div>
+            <div
+              className="-left-25 relative top-15 animate-sway"
+              style={{
+                // @ts-expect-error custom style
+                "--base-rotation": "40deg",
+                "--sway-duration": "2s",
+                "--sway-angle": "-2deg",
+              }}
+            >
+              <Image src={leaf} alt="leaf" width={180} height={373} />
+            </div>
+          </div>
+          <div className="text-[#c53974] font-semibold text-4xl text-center">
+            <h3>Venue</h3>
+          </div>
+          <div className="py-5">
+            <p className="text-xl text-center text-[#728e1c]">
+              Buddha Heritage Resort
+            </p>
+            <p className="text-sm text-center text-[#728e1c] mt-2">
+              Tourist Complex, opposite Maya sarovar park,
+            </p>
+            <p className="text-sm text-center text-[#728e1c]">
+              near Thai Temple, Bodh Gaya, Bihar 824231
+            </p>
+          </div>
+        </div>
+        <div>
+          <div className="text-[#c53974] font-semibold text-4xl text-center pb-20">
+            <h3>Events</h3>
+          </div>
+          <div>
+            <Image
+              src={haldi}
+              alt="Haldi"
+              width={280}
+              className="mx-auto relative z-2"
+            />
+            <Image
+              src={hangingFlower}
+              alt="Haldi"
+              width={155}
+              className="mx-auto relative -top-6 animate-hang z-1"
+            />
+          </div>
+          <div className="text-center text-[#b29862] text-lg">
+            <p>12th Feburary</p>
+            <p>Dress Code : Yellow</p>
+          </div>
+        </div>
+        <div className="pt-20 animate-peacock">
+          <Image src={peacock} alt="peacock" width={512} className="w-[110%]" />
+        </div>
+      </div>
+    </main>
   );
 }
